70226abe4f99b7261f0f9067be1d29f163d81ad5
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-10-11 12:17:42 +0200

    public, not internal constants...

--- src/test/kotlin/no/nav/bidrag/cucumber/constants.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/constants.kt
@@ -1,10 +1,10 @@
 package no.nav.bidrag.cucumber
 
-internal const val ENVIRONMENT = "ENVIRONMENT"
-internal const val FASIT_URL = "https://fasit.adeo.no/api/v2/resources"
-internal const val FASIT_ZONE = "fss"
-internal const val OIDC_ALIAS = "bidrag-dokument-ui-oidc"
-internal const val REDIRECT_ISSO_URI = "https://bidrag-dokument-ui.nais.preprod.local/isso"
-internal const val TEST_USER = "TEST_USER"
-internal const val TEST_USER_PASSWORD = "TEST_PASS"
-internal const val X_ENHETSNUMMER_HEADER = "X-Enhetsnummer"
+const val ENVIRONMENT = "ENVIRONMENT"
+const val FASIT_URL = "https://fasit.adeo.no/api/v2/resources"
+const val FASIT_ZONE = "fss"
+const val OIDC_ALIAS = "bidrag-dokument-ui-oidc"
+const val REDIRECT_ISSO_URI = "https://bidrag-dokument-ui.nais.preprod.local/isso"
+const val TEST_USER = "TEST_USER"
+const val TEST_USER_PASSWORD = "TEST_PASS"
+const val X_ENHETSNUMMER_HEADER = "X-Enhetsnummer"
