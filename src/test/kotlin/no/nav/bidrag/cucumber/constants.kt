4aab9be4ae47096fafc5706245db6f6f962c3be3
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-10-15 15:16:41 +0200

    constants are internal

--- src/test/kotlin/no/nav/bidrag/cucumber/constants.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/constants.kt
@@ -1,16 +1,16 @@
 package no.nav.bidrag.cucumber
 
-const val ENVIRONMENT = "ENVIRONMENT"
-const val FASIT_ZONE = "fss"
-const val OIDC_ALIAS = "bidrag-dokument-ui-oidc"
+internal const val ENVIRONMENT = "ENVIRONMENT"
+internal const val FASIT_ZONE = "fss"
+internal const val OIDC_ALIAS = "bidrag-dokument-ui-oidc"
 
-const val URL_FASIT = "https://fasit.adeo.no/api/v2/resources"
-const val URL_ISSO = "https://isso-q.adeo.no:443/isso/json/authenticate?authIndexType=service&authIndexValue=ldapservice"
-const val URL_ISSO_REDIRECT = "https://bidrag-dokument-ui.nais.preprod.local/isso"
+internal const val URL_FASIT = "https://fasit.adeo.no/api/v2/resources"
+internal const val URL_ISSO = "https://isso-q.adeo.no:443/isso/json/authenticate?authIndexType=service&authIndexValue=ldapservice"
+internal const val URL_ISSO_REDIRECT = "https://bidrag-dokument-ui.nais.preprod.local/isso"
 
-const val X_ENHETSNUMMER_HEADER = "X-Enhetsnummer"
-const val X_OPENAM_PASSW_HEADER = "X-OpenAM-Password"
-const val X_OPENAM_USER_HEADER = "X-OpenAM-Username"
+internal const val X_ENHETSNUMMER_HEADER = "X-Enhetsnummer"
+internal const val X_OPENAM_PASSW_HEADER = "X-OpenAM-Password"
+internal const val X_OPENAM_USER_HEADER = "X-OpenAM-Username"
 
-const val TEST_USER = "TEST_USER"
-const val TEST_USER_PASSWORD = "TEST_PASS"
+internal const val TEST_USER = "TEST_USER"
+internal const val TEST_USER_PASSWORD = "TEST_PASS"
