202033ed693497becf79c9a3c6a5db26c9678df7
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-10-17 08:12:44 +0200

    EnvironmentEgenskap -> SikkerhetEgenskap

--- src/test/kotlin/no/nav/bidrag/cucumber/SikkerhetEgenskap.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/SikkerhetEgenskap.kt
@@ -4,7 +4,7 @@ import io.cucumber.java.no.Gitt
 import io.cucumber.java.no.Når
 import io.cucumber.java.no.Så
 
-class EnvironmentEgenskap {
+class SikkerhetEgenskap {
 
     @Gitt("jeg bruker miljø: {string}")
     fun `jeg bruker`(miljo: String) {
