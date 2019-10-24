4aaaf9ee4b42044187067212da2f4987e3590e15
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-10-24 10:36:32 +0200

    deling av kode til tross for at annotasjoner må være unike
    - wip, fremdeles feilende tester i bidrag-dokument-journalpost
    - slettet BidragCucumberEgenskap da dette er default kotlin-versjonen
    - RestTjeneste er en åpen klasse for utføring av REST operasjoner
    - slettet RestTjenesteEgenskap og gjenbruker RestTjeneste gjennom arv

---
+++ src/test/kotlin/no/nav/bidrag/cucumber/dokument/RestTjenesteDokument.kt
@@ -0,0 +1,5 @@
+package no.nav.bidrag.cucumber.dokument
+
+import no.nav.bidrag.cucumber.RestTjeneste
+
+class RestTjenesteDokument : RestTjeneste("bidragDokument")
---
+++ src/test/kotlin/no/nav/bidrag/cucumber/dokument/journalpost/RestTjenesteJournalpost.kt
@@ -0,0 +1,5 @@
+package no.nav.bidrag.cucumber.dokument.journalpost
+
+import no.nav.bidrag.cucumber.RestTjeneste
+
+class RestTjenesteJournalpost : RestTjeneste("bidragDokumentJournalpost")
