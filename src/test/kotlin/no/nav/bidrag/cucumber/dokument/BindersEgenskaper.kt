c3463d8244a2f34d717de5b2f3f0e32213a8e862
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-07 15:17:52 +0100

    fikset binders.feature
    gjenbruker BindersEgenskap med nytt navn BindersEgenskaper

--- src/test/kotlin/no/nav/bidrag/cucumber/dokument/BindersEgenskaper.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/dokument/BindersEgenskaper.kt
@@ -2,19 +2,16 @@ package no.nav.bidrag.cucumber.dokument
 
 import io.cucumber.core.api.Scenario
 import io.cucumber.java.Before
-import io.cucumber.java.no.Gitt
 import io.cucumber.java.no.Når
 import io.cucumber.java.no.Og
-import io.cucumber.java.no.Så
 import no.nav.bidrag.cucumber.BidragCucumberScenarioManager
 import no.nav.bidrag.cucumber.Environment
 import no.nav.bidrag.cucumber.Fasit
 import no.nav.bidrag.cucumber.FellesEgenskaper.Companion.restTjeneste
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertAll
-import org.springframework.http.HttpStatus
 
-class BindersEgenskap {
+class BindersEgenskaper {
 
     companion object {
         lateinit var dokumentreferanse: DokumentReferanse
@@ -28,7 +25,13 @@ class BindersEgenskap {
     @Når("jeg ber om tilgang til dokument på journalpostId {string} og dokumentreferanse {string}")
     fun `jeg ber om tilgang til dokument for en journalpost og dokumentreferanse`(journalpostId: String, dokumentreferanse: String) {
         restTjeneste.exchangeGet("/tilgang/$journalpostId/$dokumentreferanse")
-        BindersEgenskap.dokumentreferanse = DokumentReferanse(dokumentreferanse)
+        BindersEgenskaper.dokumentreferanse = DokumentReferanse(dokumentreferanse)
+    }
+
+    @Og("jeg ber om tilgang til dokument {string}")
+    fun `jeg ber om tilgang til dokument`(dokumentreferanse: String) {
+        BindersEgenskaper.dokumentreferanse = DokumentReferanse(dokumentreferanse)
+        restTjeneste.exchangeGet("/tilgang/$dokumentreferanse")
     }
 
     @Og("dokument url skal være gyldig")
