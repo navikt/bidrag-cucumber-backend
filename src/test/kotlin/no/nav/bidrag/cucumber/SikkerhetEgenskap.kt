63be65685409c1886bab97f222fbc4195d60c663
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-11 09:41:59 +0100

    use dynamic environment settings after hard coded test

--- src/test/kotlin/no/nav/bidrag/cucumber/SikkerhetEgenskap.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/SikkerhetEgenskap.kt
@@ -1,5 +1,6 @@
 package no.nav.bidrag.cucumber
 
+import io.cucumber.java.After
 import io.cucumber.java.no.Gitt
 import io.cucumber.java.no.Så
 import org.assertj.core.api.Assertions.assertThat
@@ -14,5 +15,10 @@ class SikkerhetEgenskap {
     @Så("kan vi hente id token")
     fun `sa kan vi hente id token`() {
         assertThat(Sikkerhet().fetchOnlineIdToken()).isNotEmpty()
+    }
+
+    @After
+    fun `bruk dynamisk miljo`() {
+        Environment.use(System.getProperty("ENVIRONMENT"))
     }
 }
