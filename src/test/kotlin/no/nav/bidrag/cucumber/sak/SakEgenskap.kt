b094979d55f06503304d9943e8f6f3202edb9808
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-12-09 15:43:11 +0100

    pip for sak 0000003 i  bidrag-sak

--- src/test/kotlin/no/nav/bidrag/cucumber/sak/SakEgenskap.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/sak/SakEgenskap.kt
@@ -3,8 +3,8 @@ package no.nav.bidrag.cucumber.sak
 import io.cucumber.core.api.Scenario
 import io.cucumber.java.Before
 import io.cucumber.java.no.Når
-import no.nav.bidrag.cucumber.ScenarioManager
 import no.nav.bidrag.cucumber.FellesEgenskaper
+import no.nav.bidrag.cucumber.ScenarioManager
 
 class SakEgenskap {
 
@@ -16,5 +16,10 @@ class SakEgenskap {
     @Når("jeg henter bidragssaker for person med fnr {string}")
     fun jeg_henter_bidragssaker_for_person_med_fnr(fnr: String) {
         FellesEgenskaper.restTjeneste.exchangeGet("/person/sak/$fnr")
+    }
+
+    @Når("jeg henter pip for sak {string}")
+    fun jeg_henter_pip_for_sak(saksnummer: String) {
+        FellesEgenskaper.restTjeneste.exchangeGet("/pip/sak/$saksnummer")
     }
 }
