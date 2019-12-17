834cf2817cea09ad773810fd3e051e513607c0f8
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-12-17 10:40:52 +0100

    "manage scenario" er en FellesEgenskap

--- src/test/kotlin/no/nav/bidrag/cucumber/RunCucumberTest.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/RunCucumberTest.kt
@@ -1,10 +1,7 @@
 package no.nav.bidrag.cucumber
 
-import io.cucumber.core.api.Scenario
-import io.cucumber.java.Before
 import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
-import no.nav.bidrag.cucumber.dokument.ApplikasjonEgenskap
 import org.junit.runner.RunWith
 
 /**
@@ -12,10 +9,4 @@ import org.junit.runner.RunWith
  */
 @RunWith(Cucumber::class)
 @CucumberOptions(plugin = ["pretty", "json:target/cucumber-report/cucumber.json"])
-class RunCucumberTest {
-
-    @Before
-    fun `manage scenario`(scenario: Scenario) {
-        ScenarioManager.use(scenario)
-    }
-}
+class RunCucumberTest
