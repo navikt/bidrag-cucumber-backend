12a8851ecdb8e7837b7e84fb20ab0d9a5cffc58d
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-06-10 18:34:51 +0200

    ny feature samt enkel konfigurering for lokal kjøring
    - unngår bruk av Fasit, men forventer et minimum av konfigurering i `constants.kt`
    - ny klasse for caching av RestTjeneste.kt: CacheRestTemplateMedBaseUrl.kt
    - ny klasse for enkel konfigurering: Simple.kt
    - hendelse-producer.feature
    -

--- src/test/kotlin/no/nav/bidrag/cucumber/Fasit.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/Fasit.kt
@@ -1,21 +1,13 @@
 package no.nav.bidrag.cucumber
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import no.nav.bidrag.commons.web.HttpHeaderRestTemplate
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.ssl.SSLContexts
-import org.springframework.http.HttpHeaders
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
-import java.security.cert.X509Certificate
 
 open class Fasit {
 
     companion object {
-        private val cacheRestTemplateMedBaseUrl = CacheRestTemplateMedBaseUrl()
         private var fasitTemplate = RestTemplate()
 
         internal fun buildUriString(url: String, vararg queries: String): String {
@@ -27,7 +19,7 @@ open class Fasit {
 
         internal fun hentFasitRessursSomJson(resourceUrl: String): FasitJson {
             val fasitJson = try {
-                fasitTemplate.getForObject<String>(resourceUrl, String::class.java)
+                fasitTemplate.getForObject(resourceUrl, String::class.java)
             } catch (e: ResourceAccessException) {
                 return FasitJson(true)
             }
@@ -41,40 +33,12 @@ open class Fasit {
         }
     }
 
-    internal fun hentRestTemplateFor(alias: String): RestTemplateMedBaseUrl {
-        return cacheRestTemplateMedBaseUrl.hentEllerLag(alias)
-    }
-
-    private fun initRestTemplate(alias: String): RestTemplateMedBaseUrl {
+    internal fun hentFullContextPath(alias: String): String {
         val miljo = Environment.fetch()
         val resourceUrl = buildUriString(URL_FASIT, "type=restservice", "alias=$alias", "environment=$miljo")
         val fasitRessurs = hentFasitRessurs(resourceUrl, alias, "rest")
-        val httpComponentsClientHttpRequestFactory = hentHttpRequestFactorySomIgnorererHttps()
-        val httpHeaderRestTemplate = Environment()
-                .setBaseUrlPa(HttpHeaderRestTemplate(httpComponentsClientHttpRequestFactory), fasitRessurs.url())
 
-        httpHeaderRestTemplate.addHeaderGenerator(HttpHeaders.AUTHORIZATION) { Sikkerhet().fetchIdToken() }
-
-        return RestTemplateMedBaseUrl(httpHeaderRestTemplate, fasitRessurs.url())
-    }
-
-    private fun hentHttpRequestFactorySomIgnorererHttps(): HttpComponentsClientHttpRequestFactory {
-        val acceptingTrustStrategy = { _: Array<X509Certificate>, _: String -> true }
-        val sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build()
-
-        val csf = SSLConnectionSocketFactory(sslContext)
-
-        val httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build()
-
-        val requestFactory = HttpComponentsClientHttpRequestFactory()
-
-        requestFactory.httpClient = httpClient
-
-        return requestFactory
+        return fasitRessurs.url()
     }
 
     internal fun hentFasitRessurs(resourceUrl: String, alias: String, type: String): FasitRessurs {
@@ -97,53 +61,36 @@ open class Fasit {
 
     private fun offlineStatus(type: String) = if (Environment.offline) "check fasit.offline.$type.json" else "connected to fasit.adeo.no"
 
-    private class CacheRestTemplateMedBaseUrl(private val cache: MutableMap<String, RestTemplateMedBaseUrl> = HashMap()) {
+     data class FasitRessurs(
+            internal val alias: String,
+            private val type: String,
+            private val ressurser: MutableMap<String, String?> = HashMap()
+    ) {
+        constructor(jsonMap: Map<String, *>) : this(
+                alias = jsonMap["alias"] as String,
+                type = jsonMap["type"] as String
+        ) {
+            @Suppress("UNCHECKED_CAST") val properties = jsonMap["properties"] as Map<String, String>
+            ressurser["url"] = properties["url"]
+            ressurser["issuerUrl"] = properties["issuerUrl"]
+            ressurser["agentName"] = properties["agentName"]
+            ressurser["passord.url"] = hentPassordUrl(jsonMap["secrets"])
+        }
 
-        fun hentEllerLag(alias: String): RestTemplateMedBaseUrl {
-            if (cache.containsKey(alias)) {
-                return cache[alias]!!
+        private fun hentPassordUrl(secrets: Any?): String? {
+            if (secrets != null) {
+                @Suppress("UNCHECKED_CAST") val password = (secrets as Map<String, Map<String, String?>>)["password"]
+                return if (password != null) password["ref"] else null
             }
 
-            val restTemplateMedBaseUrl = Fasit().initRestTemplate(alias)
-
-            cache[alias] = restTemplateMedBaseUrl
-
-            return restTemplateMedBaseUrl
-        }
-    }
-}
-
-class RestTemplateMedBaseUrl(val template: RestTemplate, val baseUrl: String)
-
-data class FasitRessurs(
-        internal val alias: String,
-        private val type: String,
-        private val ressurser: MutableMap<String, String?> = HashMap()
-) {
-    constructor(jsonMap: Map<String, *>) : this(
-            alias = jsonMap["alias"] as String,
-            type = jsonMap["type"] as String
-    ) {
-        @Suppress("UNCHECKED_CAST") val properties = jsonMap["properties"] as Map<String, String>
-        ressurser["url"] = properties["url"]
-        ressurser["issuerUrl"] = properties["issuerUrl"]
-        ressurser["agentName"] = properties["agentName"]
-        ressurser["passord.url"] = hentPassordUrl(jsonMap["secrets"])
-    }
-
-    private fun hentPassordUrl(secrets: Any?): String? {
-        if (secrets != null) {
-            @Suppress("UNCHECKED_CAST") val password = (secrets as Map<String, Map<String, String?>>)["password"]
-            return if (password != null) password["ref"] else null
+            return null
         }
 
-        return null
+        fun url() = ressurser["url"] ?: "ingen url for $alias"
+        fun passordUrl() = ressurser["passord.url"] ?: "ingen url for $alias"
     }
 
-    fun url() = ressurser["url"] ?: "ingen url for $alias"
-    fun passordUrl() = ressurser["passord.url"] ?: "ingen url for $alias"
-}
-
-internal class FasitJson(val json: String?, val offline: Boolean) {
-    constructor(offline: Boolean) : this(null, offline)
+    internal class FasitJson(val json: String?, val offline: Boolean) {
+        constructor(offline: Boolean) : this(null, offline)
+    }
 }
