8f2a444759a8785f5886c449294c5f598fc568e4
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-10-09 20:23:48 +0200

    find max ids when creating testdata

--- src/test/kotlin/no/nav/bidrag/cucumber/RunCucumberTest.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/RunCucumberTest.kt
@@ -4,9 +4,7 @@ import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
 import no.nav.bidrag.cucumber.dokument.RestTjenesteTestdata
 import org.junit.AfterClass
-import org.junit.BeforeClass
 import org.junit.runner.RunWith
-import org.slf4j.LoggerFactory
 
 /**
  * Runner class which looks for feature-files (in src/test/resources/no.nav.bidrag.cucumber.*) to test
@@ -15,29 +13,11 @@ import org.slf4j.LoggerFactory
 @CucumberOptions(plugin = ["pretty", "json:target/cucumber-report/cucumber.json"])
 class RunCucumberTest {
     companion object {
-        @JvmStatic
-        private val LOGGER = LoggerFactory.getLogger(RunCucumberTest::class.java)
-
-        private lateinit var response: Map<String, Any>
-        private val restTjenesteTestdata = RestTjenesteTestdata("bidragDokumentTestdata")
-
-        @BeforeClass
-        @JvmStatic
-        fun fetchMaxDatabaseIds() {
-            restTjenesteTestdata.exchangeGet("journal/max/ids")
-            response = restTjenesteTestdata.hentResponseSomMap()
-            LOGGER.info(">>>> $response <<<<")
-        }
-
         @AfterClass
         @JvmStatic
         fun deleteCreatedTestdata() {
-            val fraJpId = response["maxJournalpostId"]
-            val fraJsakId = response["maxJournalsakId"]
-            val endpointUrl = "journal/delete/fra-jp-id/$fraJpId/fra-jsak-id/$fraJsakId"
-            ScenarioManager.useScenarioForLogging = false
-            restTjenesteTestdata.exchangeDelete(endpointUrl)
-            LOGGER.info("slettet testdata (ikke mottaksregistrert: $endpointUrl")
+            val restTjenesteTestdata = RestTjenesteTestdata("bidragDokumentTestdata")
+            restTjenesteTestdata.slettOpprettedeData()
         }
     }
 }
--- src/test/kotlin/no/nav/bidrag/cucumber/dokument/RestTjenesteTestdata.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/dokument/RestTjenesteTestdata.kt
@@ -1,19 +1,43 @@
 package no.nav.bidrag.cucumber.dokument
 
 import no.nav.bidrag.cucumber.RestTjeneste
+import no.nav.bidrag.cucumber.ScenarioManager
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 
-class RestTjenesteTestdata(alias: String): RestTjeneste(alias) {
+class RestTjenesteTestdata(alias: String) : RestTjeneste(alias) {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(RestTjenesteTestdata::class.java)
+        private var idForTestdataResponse: Map<String, Any>? = null
+    }
+
     fun opprettJournalpost(journalpostJson: String) {
+        if (idForTestdataResponse == null) {
+            exchangeGet("journal/max/ids")
+            idForTestdataResponse = hentResponseSomMap()
+            ScenarioManager.log(">>>> $idForTestdataResponse <<<<")
+        }
+
         val correlationIdWithContentType = initHttpHeadersWithCorrelationIdAndEnhet()
         correlationIdWithContentType.contentType = MediaType.APPLICATION_JSON
 
         post("/journalpost", HttpEntity(journalpostJson, correlationIdWithContentType))
     }
 
-    fun exchangeDelete(endpointUrl: String) {
+    fun slettOpprettedeData() {
+        if (idForTestdataResponse != null) {
+            val fraJpId = idForTestdataResponse?.get("maxJournalpostId")
+            val fraJsakId = idForTestdataResponse?.get("maxJournalsakId")
+            val endpointUrl = "journal/delete/fra-jp-id/$fraJpId/fra-jsak-id/$fraJsakId"
+            ScenarioManager.useScenarioForLogging = false
+            exchangeDelete(endpointUrl)
+            LOGGER.info("slettet testdata (ikke mottaksregistrert: $endpointUrl")
+        }
+    }
+
+    private fun exchangeDelete(endpointUrl: String) {
         val httpEntity = httpEntity(endpointUrl)
         exchange(httpEntity, endpointUrl, HttpMethod.DELETE)
     }
