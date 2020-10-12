95c5eb8873aa3df171f7cf95411d37b46eb96769
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-10-12 12:27:07 +0200

    after not in companion object

--- src/test/kotlin/no/nav/bidrag/cucumber/After.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/After.kt
@@ -10,19 +10,9 @@ import org.junit.runner.RunWith
  */
 @RunWith(Cucumber::class)
 class After {
-    companion object {
-        @After("@bidrag-dokument")
-        @JvmStatic
-        fun deleteCreatedTestdataBd() {
-            val restTjenesteTestdata = RestTjenesteTestdata("bidragDokumentTestdata")
-            restTjenesteTestdata.slettOpprettedeData()
-        }
-
-        @After("@bidrag-dokument-journalpost")
-        @JvmStatic
-        fun deleteCreatedTestdataBdj() {
-            val restTjenesteTestdata = RestTjenesteTestdata("bidragDokumentTestdata")
-            restTjenesteTestdata.slettOpprettedeData()
-        }
+    @After("@bidrag-dokument,@bidrag-dokument-journalpost")
+    fun deleteCreatedTestdata() {
+        val restTjenesteTestdata = RestTjenesteTestdata("bidragDokumentTestdata")
+        restTjenesteTestdata.slettOpprettedeData()
     }
 }
