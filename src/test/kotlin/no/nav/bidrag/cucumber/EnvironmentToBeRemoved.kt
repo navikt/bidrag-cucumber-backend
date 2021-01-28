933601ec80f756ca73848d5e8783561b3805eebf
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-01-28 13:35:00 +0100

    EnvironmentToBeRemoved.kt is a singleton

--- src/test/kotlin/no/nav/bidrag/cucumber/EnvironmentToBeRemoved.kt
+++ src/test/kotlin/no/nav/bidrag/cucumber/EnvironmentToBeRemoved.kt
@@ -1,16 +1,14 @@
 package no.nav.bidrag.cucumber
 
-internal open class EnvironmentToBeRemoved {
+internal object EnvironmentToBeRemoved {
 
-    companion object {
-        internal val miljo by lazy { fetchSystemProperty(ENVIRONMENT, "Fant ikke miljø for kjøring") }
-        internal val naisProjectFolder: String by lazy { fetchSystemProperty(PROJECT_NAIS_FOLDER, "Det er ikke oppgitt ei mappe for nais prosjekt") }
-        internal val user: String by lazy { fetchSystemProperty(CREDENTIALS_USERNAME, "Fant ikke nav-bruker (ala [x]123456)") }
-        internal val testUser: String by lazy { fetchSystemProperty(CREDENTIALS_TEST_USER, "Fant ikke testbruker (ala z123456)") }
-        internal val userAuthentication: String by lazy { fetchSystemProperty(CREDENTIALS_USER_AUTH, "Fant ikke passord til $user") }
+    internal val miljo by lazy { fetchSystemProperty(ENVIRONMENT, "Fant ikke miljø for kjøring") }
+    internal val naisProjectFolder: String by lazy { fetchSystemProperty(PROJECT_NAIS_FOLDER, "Det er ikke oppgitt ei mappe for nais prosjekt") }
+    internal val user: String by lazy { fetchSystemProperty(CREDENTIALS_USERNAME, "Fant ikke nav-bruker (ala [x]123456)") }
+    internal val testUser: String by lazy { fetchSystemProperty(CREDENTIALS_TEST_USER, "Fant ikke testbruker (ala z123456)") }
+    internal val userAuthentication: String by lazy { fetchSystemProperty(CREDENTIALS_USER_AUTH, "Fant ikke passord til $user") }
 
-        private fun fetchSystemProperty(property: String, errorMessage: String): String {
-            return System.getProperty(property) ?: throw IllegalStateException(errorMessage)
-        }
+    private fun fetchSystemProperty(property: String, errorMessage: String): String {
+        return System.getProperty(property) ?: throw IllegalStateException(errorMessage)
     }
 }
